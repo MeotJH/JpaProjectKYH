84720449b72f2efa2cae83d933caad9515278583
Meotjh <mallangyi@naver.com>
2022-04-12 17:33:04 +0900

    상품 도메인 개발 끝

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,32 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==== 비즈니스 로직 ====//
+    /**
+     *
+     * 비즈니스 서비스 쪽에서 Setter를 통해 Entity를 조작하기보단,
+     * Entity쪽에서 해당 변수를 객체 내에서 비즈니스 로직을 통해 조작하는것이
+     * 더욱 객체지향적으로 설계하기에 이렇게 비즈니스 로직을 Entity에 넣어놓는것     * */
+    /**
+     *
+     * stock 증가
+     * **/
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if ( restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+
+    }
+
 }
