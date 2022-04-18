473f45a3d97a7b40a6689f1ac26b18181b414e12
Meotjh <mallangyi@naver.com>
2022-04-18 14:25:30 +0900

    JPA강의 끝_ TODO2개남음

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void UpateItem(Long itemId, Book param){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        //TODO Set으로 만들지 말고 Entity내에서 전부 바꾸는 함수 있게 Refac 김영한 강의 변경 감지와 병합 참조
+        //여기가 끝 finditme이 repository에서 가져온 영속개체기 때문에 자동적으로 tranactional끝날때 flush가 일어나면서
+        //dirty checking를 ( 변경감지 ) 한다.
     }
 
     public List<Item> findItems(){
